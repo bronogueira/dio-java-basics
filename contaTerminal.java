2930f6c8f96034b774dec5ae9d226acccd4e41e3
Bruno Nogueira <2786717+bronogueira@users.noreply.github.com>
2024-09-21 15:31:22 -0300

    f1rst commit

--- contaTerminal.java
+++ contaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-  public static void main(String[] args) throws Exception {
-    System.out.println('teste');
-  }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Por favor, informe os dados conforme solicitado.");
+        System.out.print("Numero da conta: ");
+        int numero = in.nextInt();
+
+        System.out.println("Agencia da conta: ");
+        String agencia = in.next();
+
+        System.out.println("Nome do Cliente: ");
+        String nomeCliente = in.next();
+
+        System.out.println("Saldo da conta: ");
+        float saldo = in.nextFloat();
+        
+        String result = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %f já está disponível para saque", nomeCliente, agencia, numero, saldo);
+
+        System.out.println(result);
+    }
 }
